1be65a7005af6e51ed7c20954fc23605615b422c
Anthony Akwetea-Mensah <raeakwetea@gmail.com>
2026-05-03 15:40:46 +0200

    Fix import paths

--- src/pipeline/build_features.py
+++ src/pipeline/build_features.py
@@ -1,4 +1,6 @@
+import sys
 import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 import pandas as pd
 
 from features.feature_extractor import extract_features
